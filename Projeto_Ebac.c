7709cb60a6f49e887a763e7f0489d6c3f907dff4
marciodev111 <michelo.com.br@gmail.com>
2024-08-01 10:48:45 -0300

    Opção saída

--- Projeto_Ebac.c
+++ Projeto_Ebac.c
@@ -127,6 +127,7 @@ int main()
         printf("\t1 - Registrar nomes\n");
         printf("\t2 - Consultar nomes\n");
         printf("\t3 - Deletar nomes\n");
+        printf("\t4 - Sair do sistema");
         printf("Opção: "); //Fim do menu
         
         scanf("%d" , &opcao); //armazenando a escolha do usuário
@@ -144,6 +145,10 @@ int main()
                 break;
             case 3:
                 deletar();
+                break;
+            case 4:
+                printf("Obrigado por utilizar o sistema");
+                return 0;
                 break;
             default :
                 printf("Essa opção não está disponivel!\n");
